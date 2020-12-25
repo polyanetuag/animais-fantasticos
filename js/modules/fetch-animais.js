76241fa7404823942bb7b2f4740c0049fcea79d7
Polyane Tuag <pollyportugues@hotmail.com>
2020-12-25 15:58:34 -0300

    anima-numeros refatorado

--- js/modules/fetch-animais.js
+++ js/modules/fetch-animais.js
@@ -1,4 +1,4 @@
-import initAnimaNumeros from './anima-numeros.js';
+import AnimaNumeros from './anima-numeros.js';
 
 export default function initFetchAnimais() {
   function createAnimal(animal) {
@@ -19,7 +19,8 @@ export default function initFetchAnimais() {
         const divAnimal = createAnimal(animal);
         numerosGrid.appendChild(divAnimal);
       });
-      initAnimaNumeros();// a animação só acontecerá depois do fetch
+      const animaNumeros = new AnimaNumeros('[data-numero]', '.numeros', 'ativo');// a animação só acontecerá depois do fetch
+      animaNumeros.init()
     } catch (erro) {
       console.log(erro);
     }
