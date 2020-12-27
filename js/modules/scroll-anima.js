03be3bd1943cfdf8c4b7aa7b9cb44fb2f38fbabc
Polyane Tuag <pollyportugues@hotmail.com>
2020-12-27 13:05:40 -0300

    animacao ao scroll refeita

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -5,24 +5,42 @@ export default class ScrollAnima {
     this.sections = document.querySelectorAll(sections);
     this.windowMetade = window.innerHeight * 0.6;
 
-    this.animaScroll = this.animaScroll.bind(this);
+    this.checkDistance = this.checkDistance.bind(this);
   }
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      // eslint-disable-next-line max-len
-      const isSectionVisible = (sectionTop - this.windowMetade) < 0; // para animar antes de chegar ao topo
-      if (isSectionVisible) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) { // para evitar o disparo desnecessário da função
-        section.classList.remove('ativo');
-      }
+  //pega a distancia de cada item em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade)
+      };
     });
   }
 
+  //verifica a distancia em cada objeto em relacao ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')){
+        item.element.classList.remove('ativo')
+      }
+    })
+  }
+
   init() {
-    this.animaScroll();
-    window.addEventListener('scroll', this.animaScroll);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
+    }
+    return this;
+  }
+
+  // para com a animação
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
   }
 }
